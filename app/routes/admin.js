18db7d505031d8077b98a3b3ef0b3f093110e215
Jonatas Eleoteio <jonatas.eleoterio@outlook.com>
2020-06-23 22:32:40 -0300

    guardando as informações dos inputs

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     app.get('/formulario_inclusao_noticia', (req,res)=>{
-        res.render('admin/form_add_noticia', {validacao: {}, noticia: {}})
+        res.render('admin/form_add_noticia', {validacao:{}, noticia: {}})
     })
 
     app.post('/noticias/salvar', (req,res)=>{
@@ -16,7 +16,7 @@ module.exports = function(app) {
         const erros = req.validationErrors()
 
         if(erros) {
-            res.render('admin/form_add_noticia', {validacao: erros, noticia: noticia})
+            res.render('admin/form_add_noticia', { validacao : erros, noticia:noticia })
             return
         }
 
--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -11,4 +11,14 @@ module.exports = (appp) => {
             res.render('noticias/noticias', {noticias : result})
         })
     })
+
+    app.get('/noticia', (req, res)=> {
+        const connection = app.config.dbConnections()
+
+        const noticiaDAO = new app.app.models.NoticiasDAO(connection)
+
+        noticiaDAO.getNoticia((error, result) => {
+            res.render('noticias/noticia', {noticia: result})
+        })
+    })
 }
